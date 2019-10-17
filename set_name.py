287afcb8625b0a56cb3160cfb0d6b3c91e6e002f
OVanRijswijk <ovanrijswijk@storans.school.nz>
2019-10-18 11:47:26 +1300

    Ask the user to set the name of the rabbit

---
+++ set_name.py
@@ -0,0 +1,2 @@
+name = input("What do you want to call your rabbit: ").strip().title()
+print(name)
---
+++ set_weight.py
@@ -0,0 +1,2 @@
+weight = float(input("Please set the weight of {}. The weight should be between 1.5 - 2.5: "))
+print(weight)
