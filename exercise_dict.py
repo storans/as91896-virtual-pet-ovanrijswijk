74c26bec9093087bd26b336ffa51b96830b47b12
OVanRijswijk <ovanrijswijk@storans.school.nz>
2019-10-22 20:42:09 +1300

    I made an exercise list to be able to use my weight_change function. I added commenting as well

--- exercise_dict.py
+++ exercise_dict.py
@@ -1,2 +1,7 @@
+# A dictionary that holds the exercise and the amount that each exercise would reduce the weight of the rabbit
 EXERCISE_DICT = {"Hopping": -0.3, "Running": -0.5, "Walking": -0.1}
 print(EXERCISE_DICT)
+
+# I realised for my weight_change function I need to have my exercise in a list not a dictionary
+EXERCISE_LIST = [["Hopping", -0.3], ["Running", -0.5], ["Walking", -0.1]]
+print(EXERCISE_LIST)
