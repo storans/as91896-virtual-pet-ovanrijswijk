19b9eeee2e85bbeaba8cf3c80f9b7b7e5a46bf76
OVanRijswijk <ovanrijswijk@storans.school.nz>
2019-10-22 14:36:23 +1300

    Made the function also check if the number is wuthin a boundary and made the testing question more relevant to the program and added coding

--- int_checker.py
+++ int_checker.py
@@ -1,13 +1,16 @@
-# A function that will check if what the user inputs is an integer, if the user something
-def check_int(question, error):
+def check_int(question, error, low, high): # I used question, error, low and high so I can use the check integer function for different things
     valid = False
 
     while valid == False:
-        number = input("{}: ".format(question))
+        number = input("{}: ".format(question)) # The question is customised to the question
         try:
-          number = int(number)
-          return number
+            number = int(number)
+            if low <= number <= high:
+                return number
+            else:
+                print(error)
         except ValueError:
-            print(error)
+            print(error) # The error message is customised to the question
 
-sport = check_int("Please enter your chosen sport", "Please enter only the number of the sport")
+# Asks the user for the weight for testing purposes
+activity = check_int("Please enter the number of the activity - make sure it is between 1 and 5", "Please enter a number between 1 and 5", 1, 5)
