478fe542ffc6c93da45934ae2b9eda1120fab5ca
OVanRijswijk <ovanrijswijk@storans.school.nz>
2019-10-22 14:33:07 +1300

    Added and changed comments

--- float_checker.py
+++ float_checker.py
@@ -1,15 +1,17 @@
-def check_float(question, error, low, high): # Question and error allow it to be used in a range of situations
+def check_float(question, error, low, high): # I used question, error, low and high so I can use the check float function for different things
     valid = False
 
     while valid == False:
-        number = input("{}: ".format(question)) # Any question that needs a number
+        number = input("{}: ".format(question)) # The question is customised to the question
         try:
             number = float(number)
             if low <= number <= high:
-                return number # Note, I do not set valid to True, as returning does this
+                return number
             else:
                 print(error)
         except ValueError:
-            print(error) # Customised to the question!
+            print(error) # The error message is customised to the question
 
+
+# Asks the user for the weight for testing purposes
 weight = check_float("Please enter the weight you want your rabbit to be - make sure the weight is between 1.5 and 2.5", "Please enter the weight between 1.5 and 2.5", 1.5, 2.5)
