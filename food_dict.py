d36b516c0721ce96f7de39dc7f51618c143a9616
OVanRijswijk <ovanrijswijk@storans.school.nz>
2019-10-22 20:42:31 +1300

    I made an food list to be able to use my weight_change function. I added commenting as well

--- food_dict.py
+++ food_dict.py
@@ -1,2 +1,7 @@
+# A dictionary that holds that food and the amount that each food would add to the rabbit
 FOOD_DICT = {"Kale": 0.1, "Broccoli": 0.2, "Apple": 0.4}
 print(FOOD_DICT)
+
+# I realised for my weight_change function I need to have my food in a list not a dictionary
+FOOD_LIST = [["Kale", 0.1], ["Broccoli", 0.2], ["Apple", 0.4]]
+print(FOOD_LIST)
