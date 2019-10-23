15334c7723edf51f30e5bdf235a1ca5f54e5b0e4
OVanRijswijk <ovanrijswijk@storans.school.nz>
2019-10-23 14:06:54 +1300

    testing the committing lkljjk khjk

---
+++ check_weight.py
@@ -0,0 +1,6 @@
+weight = float(input("Please set the weight of {}. The weight should be between 1.5 - 2.5: "))
+
+while weight < 2.5 and weight > 1.5:
+    keep_going = ""
+else:
+    print("Sorry")
