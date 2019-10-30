6dfa5df02fc84bf81a426ddfc83265f7c5ba1df2
OVanRijswijk <ovanrijswijk@storans.school.nz>
2019-10-30 16:47:26 +1300

    Checks if the game should keep going based off the rabbits weight

--- check_death.py
+++ check_death.py
@@ -11,11 +11,11 @@ if 1.5 <= weight <= 2.5:
 else:
     print("Sorry, {} died".format(name))
     if 1.5 > weight:
-        print("{} was underweight".format(name))
-        print("{}'s weight was {}".format(name, weight))
+         print("{} was underweight".format(name))
+         print("{}'s weight was {}".format(name, weight))
     if 2.5 < weight:
-        print("{} was overweight".format(name))
-        print("{}'s weight was {}".format(name, weight))
+         print("{} was overweight".format(name))
+         print("{}'s weight was {}".format(name, weight))
 
     keep_going = "g" # Game ends
     print("Game ends")
